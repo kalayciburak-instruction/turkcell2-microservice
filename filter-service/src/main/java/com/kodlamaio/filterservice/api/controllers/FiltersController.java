6c8034cb2eec6d2641d0fa2b7c66fa125ff1afe3
kalayciburak <kalayciburak1996@gmail.com>
2023-05-06 16:37:08 +0300

    06-05-23

---
+++ filter-service/src/main/java/com/kodlamaio/filterservice/api/controllers/FiltersController.java
@@ -0,0 +1,30 @@
+package com.kodlamaio.filterservice.api.controllers;
+
+import com.kodlamaio.filterservice.business.abstracts.FilterService;
+import com.kodlamaio.filterservice.business.dto.responses.GetAllFiltersResponse;
+import com.kodlamaio.filterservice.business.dto.responses.GetFilterResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/filters")
+public class FiltersController {
+    private final FilterService service;
+
+    @GetMapping
+    public List<GetAllFiltersResponse> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GetFilterResponse getById(@PathVariable UUID id) {
+        return service.getById(id);
+    }
+}
---
+++ filter-service/src/main/java/com/kodlamaio/filterservice/business/concretes/FilterManager.java
@@ -0,0 +1,60 @@
+package com.kodlamaio.filterservice.business.concretes;
+
+import com.kodlamaio.commonpackage.utils.mappers.ModelMapperService;
+import com.kodlamaio.filterservice.business.abstracts.FilterService;
+import com.kodlamaio.filterservice.business.dto.responses.GetAllFiltersResponse;
+import com.kodlamaio.filterservice.business.dto.responses.GetFilterResponse;
+import com.kodlamaio.filterservice.entities.Filter;
+import com.kodlamaio.filterservice.repository.FilterRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class FilterManager implements FilterService {
+    private final FilterRepository repository;
+    private ModelMapperService mapper;
+
+    @Override
+    public List<GetAllFiltersResponse> getAll() {
+        var filters = repository.findAll();
+        var response = filters
+                .stream()
+                .map(filter -> mapper.forResponse().map(filter, GetAllFiltersResponse.class))
+                .toList();
+
+        return response;
+    }
+
+    @Override
+    public GetFilterResponse getById(UUID id) {
+        var filter = repository.findById(id).orElseThrow();
+        var response = mapper.forResponse().map(filter, GetFilterResponse.class);
+
+        return response;
+    }
+
+    @Override
+    public void add(Filter filter) {
+        filter.setId(UUID.randomUUID());
+        repository.save(filter);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllByBrandId(UUID brandId) {
+
+    }
+
+    @Override
+    public void deleteAllByModelId(UUID modelId) {
+
+    }
+}
