ac3ed68614f53d84ce06255cef90481fab80a67c
kalayciburak <kalayciburak1996@gmail.com>
2023-05-07 19:02:58 +0300

    add api-gateway

--- api-gateway/src/main/java/com/kodlamaio/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/kodlamaio/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.kodlamaio.filterservice;
+package com.kodlamaio.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class FilterServiceApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FilterServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
