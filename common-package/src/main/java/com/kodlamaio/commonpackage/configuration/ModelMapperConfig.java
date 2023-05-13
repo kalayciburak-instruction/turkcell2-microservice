6c69c6e55210db523eddb9dd35cbcf7e879469de
kalayciburak <kalayciburak1996@gmail.com>
2023-05-13 14:52:20 +0300

    update common package

--- common-package/src/main/java/com/kodlamaio/commonpackage/configuration/ModelMapperConfig.java
+++ common-package/src/main/java/com/kodlamaio/commonpackage/configuration/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.kodlamaio.commonpackage.configuration;
 
+import com.kodlamaio.commonpackage.utils.mappers.ModelMapperManager;
+import com.kodlamaio.commonpackage.utils.mappers.ModelMapperService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +11,10 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ModelMapperService getModelMapperService(ModelMapper mapper) {
+        return new ModelMapperManager(mapper);
     }
 }
--- common-package/src/main/java/com/kodlamaio/commonpackage/utils/mappers/ModelMapperManager.java
+++ common-package/src/main/java/com/kodlamaio/commonpackage/utils/mappers/ModelMapperManager.java
@@ -3,9 +3,7 @@ package com.kodlamaio.commonpackage.utils.mappers;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.stereotype.Service;
 
-@Service
 @AllArgsConstructor
 public class ModelMapperManager implements ModelMapperService {
     private ModelMapper mapper;
