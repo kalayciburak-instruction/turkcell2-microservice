7a651ea8a2972877779a619cfb8a2f53f54d7029
kalayciburak <kalayciburak1996@gmail.com>
2023-05-24 20:35:34 +0300

    add config-server

---
+++ config-server/src/main/java/com/kodlamaio/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
